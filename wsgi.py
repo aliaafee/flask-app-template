a805935bc3a37063e95742ac8fa081cae8cfcdca
Ali Aafee <ali.aafee@gmail.com>
2021-06-02 17:03:28 +0545

    Fixed a stupid bug

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,12 @@ import os
 from app import create_app
 from config import config
 
-
-if __name__ == "__main__":
-	wsgi_app = create_app(
+app = create_app(
 		config[os.getenv('FLASK_CONFIG') or 'default']
 	)
 
-	wsgi_app.run()
+
+
+if __name__ == "__main__":
+	app.run()
 
