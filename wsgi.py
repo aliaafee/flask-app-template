aca97d317d3a967c787eef20a70e1eed6dd1a99a
Ali Aafee <ali.aafee@gmail.com>
2021-06-02 16:22:19 +0545

    add all needed files

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,13 @@
-from app.main import app
+import os
+
+from app import create_app
+from config import config
+
 
 if __name__ == "__main__":
-	app.run()
+	wsgi_app = create_app(
+		config[os.getenv('FLASK_CONFIG') or 'default']
+	)
+
+	wsgi_app.run()
 
